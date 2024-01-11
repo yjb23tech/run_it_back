d2e309dd65ea9f7934692f405336dc695c1c6dab
yjb23tech <yjb23tech@gmail.com>
2024-01-11 22:36:50 +0000

    Scarface

--- App.jsx
+++ App.jsx
@@ -1,10 +1,18 @@
 import { createRoot } from "react-dom/client"
-import NavigationBar from "./components/NavigationBar"
+import NavigationBar from "./components/NavigationBar.jsx"
+import Card from "./components/Card.jsx"
 
 const App = () => {
     return (
         <div>
             <NavigationBar />
+            <Card 
+                song_title = "Many Men"
+                album_title = "Get Rich Or Die Trying'"
+                number_of_likes = "1000 likes"
+                number_of_comments = "300 comments"
+                
+            />
         </div>
     )
 }
--- components/Card.jsx
+++ components/Card.jsx
@@ -6,26 +6,26 @@ import comment_icon from "../public/images/comment_icon.png"
 const Card = (props) => {
     return (
         <div className="ss-card">
-            <div className="ss-row-song-details">
+            <div className="ss-row-song-details ss-row-general-setup">
                 <h2 className="ss-song-title">{props.song_title}</h2>
-                <div className="ss-edit-trash-icons">
+                <div className="ss-edit-trash-icons ss-row-general-setup">
                     <img src={pencil_icon} />
                     <img src={trash_icon} />
                 </div>
             </div>
-            {/* <div className="ss-row-album-title">
+            <div className="ss-row-album-title">
                 <p>{props.album_title}</p>
             </div>
             <div className="ss-row-blank">
             </div>
-            <div className="ss-row-like-comment-text">
+            <div className="ss-row-like-comment-text ss-row-general-setup">
                 <p>{props.number_of_likes}</p>
                 <p>{props.number_of_comments}</p>
             </div>
-            <div className="ss-row-like-comment-icons">
+            <div className="ss-row-like-comment-icons ss-row-general-setup">
                 <img src={heart_icon} />
                 <img src={comment_icon} />
-            </div> */}
+            </div>
         </div>
     )
 }
