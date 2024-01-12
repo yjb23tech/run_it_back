2f095f37ee9b1058873c9418a6a4a27c7e22ea3a
yjb23tech <yjb23tech@gmail.com>
2024-01-12 22:39:01 +0000

    Gojo

--- components/NavigationBar.jsx
+++ components/NavigationBar.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom"
+
 const NavigationBar = () => {
     return (
         <nav className="nav-bar">
             <h2 className="nb-title">SongSherpa</h2>
             <div className="nb-page-options">
-                <p>Dashboard</p>
-                <p>Explorer</p>
+                <Link to={`/`} className="nb-page-options-links">
+                    <p>Dashboard</p>
+                </Link>
+                <Link to={`/explorer`} className="nb-page-options-links">
+                    <p>Explorer</p>
+                </Link>
                 <p>About</p>
             </div>
         </nav>
